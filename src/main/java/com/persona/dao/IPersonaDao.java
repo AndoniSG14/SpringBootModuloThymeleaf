f1f33aba4968a164303074c1ce76c1a34be584ea
Andoni Alexis Sánchez Guttiérrez <101168853+AndoniSG14@users.noreply.github.com>
2024-01-29 15:36:23 -0600

    Add files via upload

---
+++ src/main/java/com/persona/dao/IPersonaDao.java
@@ -0,0 +1,15 @@
+package com.persona.dao;
+
+import java.io.Serializable;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.persona.modelo.Persona;
+
+public interface IPersonaDao extends CrudRepository<Persona, Serializable> {
+
+	/*YA NO SE PROGRAMAN LAS CONSULTAS (CRUD)
+	 * CRUD REPOSITORY: SAVE, DELETE, SELECTBYID,UPDATE, ETC...
+	 * 
+	 * */
+}
